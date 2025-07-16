8b479a3a7a8bc737231901c99cd84a41278b7b38
Tatsiana Kuryla <tatianakuryla@gmail.com>
2025-07-16 17:15:09 +0200

    docs: setup deploy

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,8 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/async-race/',
   },
   mode: 'development',
   devtool: 'source-map',
